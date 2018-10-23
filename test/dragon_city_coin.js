28d91cf38fcc8f7e0e0b6d2016af1e2166e71482
liudian <hardy.liu@outlook.com>
2018-10-23 17:57:29 +0800

    update: 更新truffle.js，添加local ganache环境的配置

---
+++ test/dragon_city_coin.js
@@ -0,0 +1,7 @@
+const web3 = require('web3')
+const DragonCityCoin = artifacts.require('./dcl_coins/DragonCityCoin.sol')
+const config = require('../tools/config.js')
+
+contract('DragonCityCoin', async (accounts) => {
+  console.log(accounts)
+})
