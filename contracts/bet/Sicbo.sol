f26c40071acbe167063bec544525a09df26ab779
liudian <hardy.liu@outlook.com>
2018-11-16 16:09:29 +0800

    update sicbo合约，添加大赢家20显示方法

--- contracts/bet/Sicbo.sol
+++ contracts/bet/Sicbo.sol
@@ -276,6 +276,19 @@ contract Sicbo is Pausable {
         return top20Player_;
     }
 
+    function getTop20PlayerWin(uint256 _choice, uint256 _roundId) public view returns(address[20], uint256[20]) {
+        uint256[20] memory top20Player_ = getTop20Player(_choice, _roundId);
+        address[20] memory top20PlayerAddr_;
+        uint256[20] memory top20PlayerWin_;
+
+        for (uint256 i; i < top20Player_.length; i++) {
+            uint256 pid_ = top20Player_[i];
+            top20PlayerAddr_[i] = playersIdAddress[pid_];
+            top20PlayerWin_[i] = (getPlayerRoundWin(pid_, _roundId));
+        }
+        return (top20PlayerAddr_, top20PlayerWin_);
+    }
+
     //开大小
     function roll() private view returns(uint8) {
         bytes32 lastBlockHash = blockhash(block.number - 1);
